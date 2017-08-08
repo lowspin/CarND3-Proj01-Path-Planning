22235db9cd89abd572663079fbf5df6f238eab26
tzeping <lowspin@gmail.com>
2017-08-07 23:10:12 -0400

    add initial support files

---
+++ src/path.cpp
@@ -0,0 +1,24 @@
+
+#include "path.h"
+#include <fstream>
+#include <math.h>
+#include <uWS/uWS.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+#include "Eigen-3.3/Eigen/QR"
+#include "Eigen-3.3/Eigen/Dense"
+#include "json.hpp"
+#include "spline.h"
+
+path::path() {}
+path::~path() {}
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+void path::init() {
+}
---
+++ src/path.h
@@ -0,0 +1,16 @@
+#ifndef path_h
+#define path_h
+
+using namespace std;
+
+class path {
+public:
+
+	path();
+	virtual ~path();
+
+	void init();
+	
+};
+
+#endif // path_h
